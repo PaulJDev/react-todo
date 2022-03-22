2d74416b0728d2bec785a97749e22a4e0ac006d7
Paul <pauljavaloyesdev@gmail.com>
2022-03-22 13:28:29 +0100

    Add CreateTodo component

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,22 @@ import { Nav } from './Nav.js'
 import { Main } from './Main.js'
 import { Footer } from './Footer.js'
 import { TodoItem } from './TodoItem.js'
+import { CreateTodo } from './CreateTodo.js'
 import { AppProvider } from '../context/AppContext.js'
+
 
 const initTodos = [
   { id: 1, description: 'Hacer algo 1', tags: ['javascript'], completed: false, title: 'Esto es una descripción de prueba más larga que la que tenía antes.', priority: 'high'},
   { id: 2, description: 'Hacer algo 2', tags: ['javascript'], completed: false, title: 'My false todo 2', priority: 'medium'},
   { id: 3, description: 'Hacer algo 3', tags: ['javascript'], completed: false, title: 'My false todo 3', priority: 'low'},
+  { id: 4, description: 'Hacer algo 4', tags: ['javascript'], completed: false, title: 'My false todo 4', priority: 'low'},
+  { id: 5, description: 'Hacer algo 5', tags: ['javascript'], completed: false, title: 'My false todo 5', priority: 'medium'},
 ]
 
 function App() {
   const [todos, setTodos] = useState(initTodos)
   const [searchValue, setSearchValue] = useState('')
+  const [displayModal, setDisplayModal] = useState(false)
 
   const completeTodo = title => {
     const newTodos = todos.map(e => e.title === title ? {...e, completed: true} : e)
@@ -45,12 +50,19 @@ function App() {
   return (
       <main className='flex flex-col h-screen'>
         <Nav />
+        <CreateTodo 
+          displayModal={displayModal}
+          setDisplayModal={setDisplayModal}
+          todos={todos}
+          setTodos={setTodos}
+        />
         <Main>
           <TodoCounter todos={todosToShow.length}/>
           <TodoList
             searchValue={searchValue}
             setSearchValue={setSearchValue}
             todos={todos}
+            setDisplayModal={setDisplayModal}
           >
             {
               todosToShow.length > 0
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,17 +3,21 @@ import { IoAppsSharp, IoList } from "react-icons/io5";
 import { GrDocumentExcel } from "react-icons/gr";
 import { BsPlusSquare } from 'react-icons/bs'
 
-export const TodoList = ({ children, searchValue, setSearchValue }) => {
+export const TodoList = ({ children, searchValue, setSearchValue, setDisplayModal }) => {
+    const handleDisplayModal = () => setDisplayModal(true)
     return (
         <section className='p-3 bg-slate-50'>
             <div className='pb-2 flex justify-between'>
                 <SearchTodo searchValue={searchValue} setSearchValue={setSearchValue} />
                 <ul className='flex justify-end'>
-                    <li><button type='button'><BsPlusSquare size={25}/></button></li>
+                    <li><button type='button' onClick={handleDisplayModal}><BsPlusSquare size={25}/></button></li>
                     <li className='pl-1'><button type='button'><IoList size={25} /></button></li>
                     <li className='px-1'><button type='button'><IoAppsSharp size={23} /></button></li>
                     <li><button type='button'><GrDocumentExcel size={23} /></button></li>
                 </ul>
+            </div>
+            <div className='pb-4'>
+                <span>Order by: </span><button>Priority</button>
             </div>
             <ul>
             {children}
